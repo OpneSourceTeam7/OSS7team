581bddf48df72f202faede9b4cb1e957b3a081d3
Marlu_Davis <swon1995@gmail.com>
2020-06-03 17:06:43 +0900

    Test upload

--- TestSDK/app/src/main/java/com/example/testsdk/MainActivity.java
+++ TestSDK/app/src/main/java/com/example/testsdk/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    //Test upload
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
